84fe1a2d9f289ebde4220f82f9d54afec1809f27
AndyDallo <dallo.inbox@gmail.com>
2022-10-11 15:39:32 +0200

    second commit

--- bot.py
+++ bot.py
@@ -4,14 +4,14 @@ import tweepy
 from dotenv import load_dotenv
 load_dotenv()
 
-
+# TWITTER API KEYS
 api_key = os.getenv('API_KEY')
 api_secret = os.getenv('API_SECRET')
 bearer_token = os.getenv('BEARER_TOKEN')
 access_token = os.getenv('ACCESS_TOKEN')
 access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
 
-
+# SET UP TWITTER CLIENT
 client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
@@ -19,8 +19,13 @@ api = tweepy.API(auth)
 
 media = api.media_upload('images/img01.jpg', chunked=True)
 media_ids = [media.media_id]
+
+# READ JSON FILES
 f =  open('fugitive.json')
 data = json.load(f)
+
+
+#TWEET CONTENT
 text = """ 
 {name}
 {forename}
@@ -38,6 +43,6 @@ Search by {chargedBy}
     chargedFor= data['charges']['for'],
     chargedBy= data['charges']['by']
     )
-print(text)
-# client.create_tweet(text= text)
-# client.create_tweet(text= text, media_ids = media_ids )
+
+# PUBLISH TWEET
+client.create_tweet(text= text, media_ids = media_ids )
--- main.py
+++ main.py
@@ -1,168 +1,139 @@
 import random, math, json, os
+import requests
 import pycountry
-import aiohttp, asyncio, time, urllib.request
+import urllib.request
 from datetime import date
 
-#generate random number
 
 
+def getRandomNum(min, max):
+    return  math.floor(random.uniform(min, max))
 
-def main():
+#SELECT PAGES OF NOTICES
+def getListNotices():
 
-    # generate random number 
-    def getRandomNum(min, max):
-        return  math.floor(random.uniform(min, max))
+        randomNum = getRandomNum(1, 500)
+        url_notices ="https://ws-public.interpol.int/notices/v1/red?resultPerPage=100&page={0}".format(randomNum)
+        response_notices =   requests.get(url_notices)
+        data_notices =   response_notices.json()
+        return data_notices
 
-    #get list of fugitives from interpol API
-    async def getListNotices():
+# GET FUGITIVE KEY
+def getNoticesDetails():
 
-        async with aiohttp.ClientSession() as session:
-            randomNum = getRandomNum(1, 200)
-            url_notices ="https://ws-public.interpol.int/notices/v1/red?resultPerPage=100&page={0}".format(randomNum)
-            response_notices =  await session.get(url_notices)
-            data_notices =  await response_notices.json()
-            return data_notices
+        data_notices =  getListNotices()
+        fugitiveKey = list(data_notices['_embedded']["notices"])
+        return fugitiveKey
 
-    asyncio.run(getListNotices())  
+#GET RANDOM FUGITIVES INDEX
+def getFugitiveIndex():
 
-    async def getNoticesDetails():
-        async with aiohttp.ClientSession() as session:
-            data_notices = await getListNotices()
-            fugitiveKey = list(data_notices['_embedded']["notices"])
-            return fugitiveKey
-    asyncio.run(getNoticesDetails())
+    fugitiveKey =  getNoticesDetails()
+    fugitivesLength = len(fugitiveKey)
+    randomIndex = getRandomNum(0, fugitivesLength)
+    def closure_func():
+        return randomIndex
+    return closure_func()
 
-    async def getFugitiveIndex():
-        async with aiohttp.ClientSession() as session:
-            
-            fugitiveKey = await getNoticesDetails()
-            fugitivesLength = len(fugitiveKey)
-            randomIndex = getRandomNum(0, fugitivesLength)
-            return randomIndex
+randomIndex = getFugitiveIndex()
 
-    asyncio.run(getFugitiveIndex())
+#GET FUGITIVES ID, ie 23456-23456
+def getFugitiveID():
 
-    async def getFugitiveID():
-        async with aiohttp.ClientSession() as session:
+        fugitiveKey =  getNoticesDetails()
+        # change  ie: 3545/3454 to 3545-3454 
+        iD = fugitiveKey[randomIndex]['entity_id']
+        iDStr = iD.split('/')
 
-            randomIndex = await getFugitiveIndex()
-            fugitiveKey = await getNoticesDetails()
+        alt_iD = list(iDStr)
+        new_iD = []
+        real_iD = list(new_iD)
 
-            # change  ie: 3545/3454 to 3545-3454 
-            iD = fugitiveKey[randomIndex]['entity_id']
-            iDStr = iD.split('/')
+        real_iD.insert(1, alt_iD[0])
+        real_iD.insert(2, '-')
+        real_iD.insert(3, alt_iD[1])
 
-            alt_iD = list(iDStr)
-            new_iD = []
-            real_iD = list(new_iD)
+        fugitive_ID = ''.join(real_iD)
+        return fugitive_ID
 
-            real_iD.insert(1, alt_iD[0])
-            real_iD.insert(2, '-')
-            real_iD.insert(3, alt_iD[1])
+# GET INFORMATIONS FROM SPECIFIC FUGITIVE
+def getFugitiveInfo():
 
-            fugitive_ID = ''.join(real_iD)
-            return fugitive_ID
+        fugitive_ID =  getFugitiveID()
+        url_fugitive_info = "https://ws-public.interpol.int/notices/v1/red/{0}".format(fugitive_ID)
+        response_fugitive_info =  requests.get(url_fugitive_info)
+        data_fugitive_info =  response_fugitive_info.json()
+        return data_fugitive_info
 
-    asyncio.run(getFugitiveID())
+#TRANSLATE ie FR TO FRANCE
+def alphaCodeToCountryOfBirth():
 
-    async def getFugitiveImages():
-        async with aiohttp.ClientSession() as session:
+        data_fugitive_info =  getFugitiveInfo()
+        country_of_birth = data_fugitive_info['country_of_birth_id']
+        countries = list(pycountry.countries)
+        for index, country in enumerate(countries):
+            if country_of_birth in country.alpha_2:
+                country_of_birth = country.name
+                return country_of_birth
 
-            randomIndex = await getFugitiveIndex()
-            fugitiveKey = await getNoticesDetails()
-            url_images = fugitiveKey[randomIndex]['_links']['images']['href']
-            response_images = await session.get(url_images)
-            data_images =  await response_images.json()
-            imageLink = data_images['_embedded']['images'][0]['_links']['self']['href']
-            return imageLink
-            
-    asyncio.run(getFugitiveImages())
+#TRANSLATE ie FR TO FRANCE
+def alphaCodeToCountryWarrant():
 
-    async def getFugitiveInfo():
-        async with aiohttp.ClientSession() as session:
+        data_fugitive_info =  getFugitiveInfo()
+        country_warant = data_fugitive_info['arrest_warrants'][0]['issuing_country_id']
+        countries = list(pycountry.countries)
+        for index, country in enumerate(countries):
+            if country_warant in country.alpha_2:
+                country_warant = country.name
+                return country_warant
 
-            fugitive_ID = await getFugitiveID()
-            url_fugitive_info = "https://ws-public.interpol.int/notices/v1/red/{0}".format(fugitive_ID)
-            response_fugitive_info = await session.get(url_fugitive_info)
-            data_fugitive_info = await response_fugitive_info.json()
-            return data_fugitive_info
+#TGET FUGITIVES IMAGES LINK
+def getFugitiveImages():
 
-    asyncio.run(getFugitiveInfo())
+        fugitiveKey =  getNoticesDetails()
+        url_images = fugitiveKey[randomIndex]['_links']['images']['href']
+        response_images =  requests.get(url_images)
+        data_images =   response_images.json()
+        imageLink = data_images['_embedded']['images'][0]['_links']['self']['href']
+        return imageLink
 
-    # change FR to FRANCE
-    async def alphaCodeToCountryOfBirth():
-        async with aiohttp.ClientSession() as session:
+# DOWNLOAD IMAGE
+def imageDownloader():
 
-            data_fugitive_info = await getFugitiveInfo()
-            country_of_birth = data_fugitive_info['country_of_birth_id']
-            countries = list(pycountry.countries)
-            for index, country in enumerate(countries):
-                if country_of_birth in country.alpha_2:
-                    country_of_birth = country.name
-                    return country_of_birth
+        url_image =  getFugitiveImages()
+        file_path = 'images/'
+        file_name = 'img01'
+        full_path = file_path + file_name + '.jpg'
+        urllib.request.urlretrieve(url_image, full_path)
 
-    asyncio.run(alphaCodeToCountryOfBirth())
+imageDownloader()
 
-    async def alphaCodeToCountryWarrant():
-        async with aiohttp.ClientSession() as session:
+# DATE OF BIRTH TO INTEGER YEARS OLD 
+def findAge():
+    
+        fugitiveInfo =  getFugitiveInfo()
+        date_of_birth = int(fugitiveInfo['date_of_birth'].split('/')[0])
+        year = date.today().year
+        age = year - date_of_birth
+        return age
 
-            data_fugitive_info = await getFugitiveInfo()
-            country_warant = data_fugitive_info['arrest_warrants'][0]['issuing_country_id']
-            countries = list(pycountry.countries)
-            for index, country in enumerate(countries):
-                if country_warant in country.alpha_2:
-                    country_warant = country.name
-                    return country_warant
+# DICT TO JSON FORMAT
+def InfoToJson():
 
-    asyncio.run(alphaCodeToCountryWarrant())
+        data_fugitive_info =  getFugitiveInfo()
+        fugitive = {
+        "name": data_fugitive_info['name'],
+        "forename" : data_fugitive_info['forename'],
+        'age' :  findAge(),
+        "nat" :  alphaCodeToCountryOfBirth(),
+        "marks" : data_fugitive_info['distinguishing_marks'] ,
+        "charges" : {
+            'for': data_fugitive_info['arrest_warrants'][0]['charge'],
+            "by" :  alphaCodeToCountryWarrant()
+        },
+        'image':  getFugitiveImages()
+        } 
+        with open('fugitive.json', 'w') as f:
+            json.dump(fugitive, f)
 
-    async def imageDownloader():
-        async with aiohttp.ClientSession() as session:
-
-            url_image = await getFugitiveImages()
-            file_path = 'images/'
-            file_name = 'img01'
-            full_path = file_path + file_name + '.jpg'
-            urllib.request.urlretrieve(url_image, full_path)
-
-    asyncio.run(imageDownloader()) 
-
-    async def findAge():
-        async with aiohttp.ClientSession() as session:
-
-            fugitiveInfo = await getFugitiveInfo()
-            randomIndex = await getFugitiveIndex()
-            
-            date_of_birth = int(fugitiveInfo['date_of_birth'].split('/')[0])
-            year = date.today().year
-            age = year - date_of_birth
-            return age
-
-    asyncio.run(findAge()) 
-
-    async def InfoToJson():
-        async with aiohttp.ClientSession() as session:
-
-            data_fugitive_info = await getFugitiveInfo()
-            fugitive = {
-            "name": data_fugitive_info['name'],
-            "forename" : data_fugitive_info['forename'],
-            'age' : await findAge(),
-            "nat" : await alphaCodeToCountryOfBirth(),
-            "marks" : data_fugitive_info['distinguishing_marks'] ,
-            "charges" : {
-                'for': data_fugitive_info['arrest_warrants'][0]['charge'],
-                "by" : await alphaCodeToCountryWarrant()
-            },
-            'image': await getFugitiveImages()
-            } 
-            with open('fugitive.json', 'w') as f:
-                json.dump(fugitive, f)
-
-    asyncio.run(InfoToJson()) 
-
-
-main()
-
-
-            
+InfoToJson()
